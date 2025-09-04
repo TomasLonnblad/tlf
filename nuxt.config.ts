88484838a7f9c4b4a7a5950d0cf5bde11b4c7067
TomasLonnblad <tomaslonnblad@gmail.com>
2025-09-04 10:44:05 +0200

    x

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+
+
+
+
+
+
+export default defineNuxtConfig({
+
+
+
+
+  modules: [
+    '@nuxt/image',
+  ],
+  app: {
+    head: {
+      title: 'Nuxt Image Example',
+    },
+  },
+  image: {
+    domains: [
+      'https://images.unsplash.com',
+      'https://source.unsplash.com',
+    ],
+  },
+})
